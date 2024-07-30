9fb4c58c9b0091ca648bc13763b807ee649fedf3
shubham <152521774+Shubh1127@users.noreply.github.com>
2024-07-30 21:26:31 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -2,10 +2,26 @@ const sideMenu=document.querySelector("aside");
 const menuBtn=document.querySelector("#menu-btn");
 const closebtn=document.querySelector("#close-btn");
 
+
+
+
+
 menuBtn.addEventListener('click',()=>{
     sideMenu.style.display='block';
 })
 
-closebtn.addEventListener('click',()=>{
-    sideMenu.style.display='none';
-})
+
+
+    const lightButton = document.querySelector(".light");
+    const darkButton = document.querySelector(".dark");
+
+    darkButton.addEventListener("click", () => {
+      console.log("Dark button clicked");
+
+      document.body.classList.add("dark-theme");
+    });
+
+    lightButton.addEventListener("click", () => {
+      console.log("Light button clicked");
+      document.body.classList.remove("dark-theme");
+    });
